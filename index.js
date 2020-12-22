989b908a2663d781968af54632ccb36ddc63170b
amm834 <aungmyatmoe.edu@gmail.com>
2020-12-23 01:22:03 +0630

    Server Setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const server = http.createServer((req,res)=>{
+ res.writeHead(200,{'Content-Type':'text/html'});
+ res.write("<h1>Hello World</h1>");
+});
+server.listen(3000,()=>{
+ console.log('Server is running!');
+});
