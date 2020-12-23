fde53dd81b3a0b43532a2e3f6562e0d91ccbe9a7
amm834 <aungmyatmoe.edu@gmail.com>
2020-12-23 23:27:23 +0630

    Added Query String

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
 const http = require("http");
 const url = require("url");
+const qs = require('querystring');
 require('dotenv').config();
+
+const responder = (req, res, params)=> {
+ res.writeHead(200, {
+  'Content-Type': 'text/html'
+ });
+ res.end(params);
+};
+
 const routes = {
  GET: {
-  '/': (req, res,params)=> {
-   res.writeHead(200, {
-    'Content-Type': 'text/html'
-   });
-   res.end('<h1> / => route and get method.');
+  '/': (req, res)=> {
+   responder(req, res, '<h1> / => route and get method.');
   },
-  '/about': (req, res,params)=> {
-   res.writeHead(200, {
-    'Content-Type': 'text/html'
-   });
-   res.end('<h1> /about => route and get method.');
+  '/about': (req, res)=> {
+   responder(req, res, '<h1> /about => route and get method.');
   },
  },
+ 
  POST: {
-  '/': (req, res,params)=> {
-   res.writeHead(200, {
-    'Content-Type': 'text/html'
+  
+  '/api/login':(req,res)=>{
+   let body = '';
+   req.on('data',data=>{
+    body += data;
    });
-   res.end(`<h1> / => route and post method. ${params.query.name}`);
-  },
-  '/about': (req, res,params)=> {
-   res.writeHead(200, {
-    'Content-Type': 'text/html'
-   });
-   res.end('<h1> /about => route and post method.');
-  },
+   req.on('end',()=>{
+    let reqData = qs.parse(body);
+    console.log(reqData);
+    res.end();
+   })
+  }
+  
  },
- NA:(req,res)=>{
- res.writeHead(404);
- res.end('<h1>404 Not Found!</h1>');
+ 
+ NA: (req, res)=> {
+  responder('<h1>404 Not Found!</h1>');
  }
 };
 
 const start = (req, res)=> {
  let requestMethod = req.method;
  let params = url.parse(req.url, true);
- 
  let resolveRoute = routes[requestMethod][params.pathname];
- if(resolveRoute != null && resolveRoute != undefined){
-  resolveRoute(req,res,params);
- }else{
-  routes['NA'](req,res,params);
+ if (resolveRoute != null && resolveRoute != undefined) {
+  resolveRoute(req, res, params);
+ } else {
+  routes['NA'](req, res, params);
  }
 };
 
