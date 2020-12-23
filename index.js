20c6ef2a4a8d7f09edc735030a907a3b435e1135
amm834 <aungmyatmoe.edu@gmail.com>
2020-12-23 21:01:36 +0630

    Route Setup

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const http = require("http");
 
+const routes = {
+ GET:{
+  '/':()=>{
+   console.log(' / path and get method')
+  },
+  '/about':()=>{
+   console.log(' /about path and get method')
+  },
+ },
+ POST:{
+  '/':()=>{
+   console.log('/ path and  post method')
+  },
+  '/about':()=>{
+   console.log('/about path and  post method')
+  },
+ }
+};
+
 const start = (req, res)=> {
  res.writeHead(200, {
   'Content-Type': 'text/html'
  });
- if(req.method == 'GET'){
- res.end('GET Method');
- }else if(req.method == 'POST'){
-  res.end('POST');
- }else{
-  res.end('LoL');
- }
+  let requestMethod = req.method;
+  let requestPath = req.url;
+  routes[requestMethod][requestPath]();
 };
 
-const server = http.createServer(start);
 
+
+const server = http.createServer(start);
 server.listen(3000, ()=> {
  console.log('Server is running!');
 });
