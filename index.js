14897b4e98c8f0197dc647ee48e9d8e8e687a407
amm834 <aungmyatmoe.edu@gmail.com>
2020-12-23 21:54:02 +0630

    Git Init

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const http = require("http");
 const url = require("url");
-
+require('dotenv').config();
 const routes = {
  GET: {
-  '/': (req, res)=> {
+  '/': (req, res,params)=> {
    res.writeHead(200, {
     'Content-Type': 'text/html'
    });
    res.end('<h1> / => route and get method.');
   },
-  '/about': (req, res)=> {
+  '/about': (req, res,params)=> {
    res.writeHead(200, {
     'Content-Type': 'text/html'
    });
@@ -17,30 +17,40 @@ const routes = {
   },
  },
  POST: {
-  '/': (req, res)=> {
+  '/': (req, res,params)=> {
    res.writeHead(200, {
     'Content-Type': 'text/html'
    });
-   res.end('<h1> / => route and post method.');
+   res.end(`<h1> / => route and post method. ${params.query.name}`);
   },
-  '/about': (req, res)=> {
+  '/about': (req, res,params)=> {
    res.writeHead(200, {
     'Content-Type': 'text/html'
    });
    res.end('<h1> /about => route and post method.');
   },
+ },
+ NA:(req,res)=>{
+ res.writeHead(404);
+ res.end('<h1>404 Not Found!</h1>');
  }
 };
 
 const start = (req, res)=> {
  let requestMethod = req.method;
- let requestPath = url.parse(req.url, true);
- routes[requestMethod][requestPath.pathname](req, res);
+ let params = url.parse(req.url, true);
+ 
+ let resolveRoute = routes[requestMethod][params.pathname];
+ if(resolveRoute != null && resolveRoute != undefined){
+  resolveRoute(req,res,params);
+ }else{
+  routes['NA'](req,res,params);
+ }
 };
 
 
 
 const server = http.createServer(start);
-server.listen(3000, ()=> {
- console.log('Server is running!');
+server.listen(process.env.PORT, ()=> {
+ console.log(`Server is running at ${process.env.PORT}`);
 });
