8445d9c46d8233744c9044f015cc9137f550cb40
amm834 <aungmyatmoe.edu@gmail.com>
2020-12-24 03:46:11 +0630

    check html file and resoonse

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require("http");
 const url = require("url");
 const qs = require('querystring');
+const fs = require('fs');
 require('dotenv').config();
+
 
 const responder = (req, res, params)=> {
  res.writeHead(200, {
@@ -10,42 +12,64 @@ const responder = (req, res, params)=> {
  res.end(params);
 };
 
+const myFileReader = (filePath, res)=> {
+ fs.access(filePath, fs.constants.F_OK, (err)=> {
+  if (err) {
+   throw err;
+  } else {
+   fs.readFile(filePath, (err, data)=> {
+    if (err) throw err;
+    res.writeHead(200, {
+     'Content-Type': 'text/html'
+    });
+    res.end(data);
+   })
+  }
+ });
+};
+
 const routes = {
  GET: {
-  '/': (req, res)=> {
-   let fileName = __dirname + '/index.html';
-   responder(req, res, fileName);
+  '/': (req,
+   res)=> {
+   let filePath = __dirname + '/index.html';
+   myFileReader(filePath, res);
   },
-  '/index.html': (req, res)=> {
-   let fileName = __dirname + '/index.html';
-   responder(req, res, fileName)
+  '/index.html': (req,
+   res)=> {
+   let filePath = __dirname + '/index.html';
+   myFileReader(filePath, res)
   }
  },
 
  POST: {
-
-  '/api/login': (req, res)=> {
+  '/api/login': (req,
+   res)=> {
    let body = '';
-   req.on('data', data=> {
-    body += data;
-   });
-   req.on('end', ()=> {
-    let reqData = qs.parse(body);
-    console.log(reqData);
-    res.end();
-   })
+   req.on('data',
+    data=> {
+     body += data;
+    });
+   req.on('end',
+    ()=> {
+     let reqData = qs.parse(body);
+     console.log(reqData);
+     res.end();
+    })
   }
 
  },
 
- NA: (req, res)=> {
+ NA: (req,
+  res)=> {
   responder('<h1>404 Not Found!</h1>');
  }
 };
 
 const start = (req, res)=> {
  let requestMethod = req.method;
- let params = url.parse(req.url, true);
+ let params = url.parse(req.url,
+  true);
  let resolveRoute = routes[requestMethod][params.pathname];
  if (resolveRoute != null && resolveRoute != undefined) {
   resolveRoute(req, res);
