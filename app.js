c53090f74b30edcf0da88dfcc0da86ec8f69d78e
amm834 <aungmyatmoe.edu@gmail.com>
2020-12-25 02:34:29 +0630

    Use Exoress Static File

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-var hasher = require("./hashpass/index");
+require("dotenv").config();
+const path = require('path')
+const express = require('express')
+const app = express();
 
-hasher.makeHash('123')
-.then(hash=>hasher.checkPasswd('123',hash))
-.then(result=>console.log(result))
-.catch(err=>console.log(err));
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function(req, res) {
+ res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.get('/index', (req, res)=> {
+ res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/about', (req, res)=> {
+ res.sendFile(__dirname + '/about.html')
+})
+
+app.listen(process.env.PORT, ()=> {
+ console.log('Server Started!')
+});
