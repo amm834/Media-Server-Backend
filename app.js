c0e820cfad31df214cbe34f6a02598a8a061784a
amm834 <aungmyatmoe.edu@gmail.com>
2020-12-26 00:58:55 +0630

    Use Body Parser

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 require("dotenv").config();
-const path = require('path')
-const express = require('express')
+const path = require('path');
+var bodyParser = require('body-parser');
+const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+//app.use(express.static(path.join(__dirname)));
 
-app.get('/', function(req, res) {
+app.use(express.static(__dirname));
+
+// body paraser
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.post('/api/login',(req,res)=>{
+ console.log(req.body);
+ res.send(`Email is ${req.body.email} and Pssword is ${req.body.passwd}`)
+})
+
+app.get('/', (req, res) => {
  res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 
 app.get('/index', (req, res)=> {
  res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/about', (req, res)=> {
- res.sendFile(__dirname + '/about.html')
+//get query params
+app.get('/api/user/',(req,res)=>{
+ console.log(req.query);
 })
+
+//get param
+app.get('/api/user/:name',(req,res)=>{
+ console.log(req.params);
+})
+
+
 
 app.listen(process.env.PORT, ()=> {
  console.log('Server Started!')
