b73a1d94640eeb35de61be64dcde39a1b1f9a64f
amm834 <aungmyatmoe.edu@gmail.com>
2020-12-26 22:58:17 +0630

    Chat Box Setup

--- app.js
+++ app.js
@@ -1,49 +1,16 @@
 require("dotenv").config();
-const path = require('path');
-var bodyParser = require('body-parser');
-const express = require('express');
-const app = express();
-var hogan = require('hogan-express');
-
-app.engine('html',hogan);
-app.set('view engine','html');
-
-//app.use(express.static(path.join(__dirname)));
-
+const express = require('express'),
+app = express(),
+server = require('http').createServer(app),
+io = require('socket.io')(server),
+hogan = require('hogan-express');
 app.use(express.static(__dirname));
-
-// body paraser
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json());
+app.engine('html', hogan);
+app.set('view engine', 'html');
 
 
-app.post('/api/login',(req,res)=>{
- console.log(req.body);
- res.send(`Email is ${req.body.email} and Pssword is ${req.body.passwd}`)
-})
-
-app.get('/', (req, res) => {
- res.render('index');
-});
-
-app.get('/index', (req, res)=> {
+app.get('/',(req,res)=>{
  res.render('index')
-})
-
-//get query params
-app.get('/api/user/',(req,res)=>{
- console.log(req.query);
-})
-
-//get param
-app.get('/api/user/:name',(req,res)=>{
- console.log(req.params);
-})
-
-
-
-app.listen(process.env.PORT, ()=> {
- console.log('Server Started!')
 });
+
+server.listen(process.env.PORT);
